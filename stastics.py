780d41fde171fe8aeff5904f8ff578d2985cb43d
suddhu1 <pathaksudarshan27@gmail.com>
2024-10-08 17:52:34 +0545

    basic on statistics

---
+++ stastics.py
@@ -0,0 +1,59 @@
+import pandas as pd 
+import numpy as np 
+from statistics import mode 
+import statistics
+
+# data2 = pd.read_csv("https://raw.githubusercontent.com/sunnysavita10/Statistics-With-Python-CompleteGuide/refs/heads/main/Iris.csv")
+# # print(data)
+
+# data1 = [23,24,32,45,12,43,67,45,32,56,32]
+# print(data1)
+
+# data3 = pd.read_csv("https://raw.githubusercontent.com/sunnysavita10/Statistics-With-Python-CompleteGuide/refs/heads/main/Titanic.csv")
+# # print(data3)
+
+# library required for the statistic pratical implement in python 
+# scipy
+# statmodel 
+# statistics
+# pandas 
+# numpy
+# matplotlib 
+# seaborn
+
+# data_copy = data1.copy()
+# store = np.mean(data1)
+# print(store)
+
+# calculating mean by using the logic 
+# s=0
+# for i in data_copy:
+#     s= s+i
+#     mean = s/len(data_copy)
+# print(mean)
+
+# simple way 
+# print(sum(data_copy)/len(data_copy))
+
+
+# print(np.median(data_copy))
+# print(mode(data_copy))
+# print(np.var(data_copy)) # population variance 
+# print(statistics.variance(data_copy)) # sample variance 
+
+
+# print(np.random.choice(11,20))  # for generating the 20  numbers array from 0 to 11
+
+# print(np.random.randint(10,20,50)) # generate the random 50 integers between the 10 and 20 
+
+
+
+
+
+
+
+
+
+
+
+
