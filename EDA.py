7829bf992542e7a2b6260f818ac32fd031f58501
suddhu1 <pathaksudarshan27@gmail.com>
2024-11-08 01:02:53 +0545

    some more knowledge

--- EDA.py
+++ EDA.py
@@ -1,7 +1,10 @@
 import pandas as pd 
+import numpy as np 
 # this data is from the googleplay store 
 df = pd.read_csv("https://raw.githubusercontent.com/sunnysavita10/Statistics-With-Python-CompleteGuide/refs/heads/main/googleplaystore.csv")
-print(df)
+data_copy = df.copy()
+
+# print(df)
 # 
 # our data fram over which we are going to do alalysis 
 
@@ -63,13 +66,66 @@ print(df)
 # rectifing the data to the respective format and data type for possible column 
 
 
-# deleting duplicate data 
-data = df.drop_duplicates( ) # dropping the duplicate value and storing the unique in the data variable 
-# print(df.shape) # shape of orginal daraframe 
-# print(data.shape) # shape of dataframe after cleaning the duplicate value 
+# data = data_copy['Price'].unique() # getting info about the column we want to clean or make as per our requirement 
+# print(data)
+
+# Price column 
+# ['0' '$4.99' '$3.99' '$6.99' '$1.49' '$2.99' '$7.99' '$5.99' '$3.49'
+#  '$1.99' '$9.99' '$7.49' '$0.99' '$9.00' '$5.49' '$10.00' '$24.99'
+#  '$11.99' '$79.99' '$16.99' '$14.99' '$1.00' '$29.99' '$12.99' '$2.49'
+#  '$10.99' '$1.50' '$19.99' '$15.99' '$33.99' '$74.99' '$39.99' '$3.95'
+#  '$4.49' '$1.70' '$8.99' '$2.00' '$3.88' '$25.99' '$399.99' '$17.99'
+#  '$400.00' '$3.02' '$1.76' '$4.84' '$4.77' '$1.61' '$2.50' '$1.59' '$6.49'
+#  '$1.29' '$5.00' '$13.99' '$299.99' '$379.99' '$37.99' '$18.99' '$389.99'
+#  '$19.90' '$8.49' '$1.75' '$14.00' '$4.85' '$46.99' '$109.99' '$154.99'
+#  '$3.08' '$2.59' '$4.80' '$1.96' '$19.40' '$3.90' '$4.59' '$15.46' '$3.04'
+#  '$4.29' '$2.60' '$3.28' '$4.60' '$28.99' '$2.95' '$2.90' '$1.97'
+#  '$200.00' '$89.99' '$2.56' '$30.99' '$3.61' '$394.99' '$1.26' 'Everyone'
+#  '$1.20' '$1.04']
+
+data_copy["Price"] = data_copy["Price"].str.replace("$","")
+data_copy["Price"] = data_copy["Price"].str.replace("Everyone",str(np.nan))
+data_copy["Price"] = data_copy["Price"].astype("float")
+
+# matching the data of the column size to the single unite 
+# here we assume 1M = 1000
+data_copy["Size"] = data_copy["Size"].str.replace("M","000") # converting mb into kb 
+data_copy["Size"] = data_copy["Size"].str.replace("k","")
+data_copy["Size"] = data_copy["Size"].str.replace('Varies with device', str(np.nan)) # non numeroic data concertes to nan 
+data_copy["Size"] = data_copy["Size"].str.replace("1,000+",str(np.nan))
+data_copy["Size"] = data_copy["Size"].astype("float")
+
+# print(data_copy["Size"].unique()) # to see the optimized Size column 
 
 
-# converting the datatype of the reviews column into the int type 
+# correcting the curernt version column to get the numeric value 
+data_copy["Current Ver"] = data_copy["Current Ver"].str.replace("Varies with device", str(np.nan))
+
+# print(data_copy["Current Ver"].isnull().sum())  # getting the info about the null value in the column 
 
 
+# correcting the androis ver ccolumn as per our requirement 
+data_copy["Android Ver"] = data_copy["Android Ver"].str.replace("and up","")
+data_copy["Android Ver"] = data_copy["Android Ver"].str.replace("Varies with device", str(np.nan))
+data_copy["Android Ver"] = data_copy["Android Ver"].str.replace("W","")
+# print(data_copy["Android Ver"].unique())
 
+# correcting the Install colimn 
+data_copy["Installs"] = data_copy["Installs"].str.replace("+","")
+data_copy["Installs"] = data_copy["Installs"].str.replace(",","")
+# print(data_copy["Installs"].unique())
+data_copy["Installs"] = data_copy["Installs"].str.replace("Free",str(np.nan))
+data_copy["Installs"] = data_copy["Installs"].astype("float")
+# print(data_copy["Installs"].unique())
+
+# Convert the date column to datetime format
+data_copy['Last Updated'] = pd.to_datetime(data_copy['Last Updated'], format='%B %d, %Y',errors='coerce')
+# print(data_copy.loc[1348 ,"Last Updated"])
+
+# Extract year, month, and day into separate columns
+data_copy['year'] = data_copy['Last Updated'].dt.year
+data_copy['month'] = data_copy['Last Updated'].dt.month
+data_copy['day'] = data_copy['Last Updated'].dt.day
+
+#saving the cleaned data to the new csv file 
+data_copy.to_csv("cleaned_googleplaystore_data.csv", index= False)
---
+++ EDA_cleaned_data.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+data = pd.read_csv("cleaned_googleplaystore_data.csv")
+# print(data )
+
+data_copy = data.copy()
+data_copy = data_copy.drop_duplicates(subset=['App'],keep="first")
+data_copy = data_copy.reset_index(drop=True)
+
+# print(data_copy.info())
+# print(data_copy)
+
+# print(data_copy["Category"].unique())
+index_values = data_copy.index[data_copy['Category'] == '1.9'].tolist()
+# print(index_values)
+data_copy = data_copy.drop(data_copy.index[index_values])
+data_copy = data_copy.reset_index(drop=True)
+# print(data_copy.groupby(["Category"])["Installs"].sum().sort_values(ascending=False))  # to get the info about the category with the most instal
+
+
+# doing analysis 
+# df_cat_install = data_copy.groupby(["Category"])["Installs"].sum().sort_values(ascending=False)[:10]
+# df_cat_install = df_cat_install.reset_index()
+# print(df_cat_install)
+
+# visiulation of the analysis 
+# sns.barplot(x="Installs",y="Category",data = df_cat_install)
+# plt.show()
+
+# top 5 app in the catagory with the most installs (multivariate analysis )
+df_cat_app_install  = data_copy.groupby(["Category","App"])["Installs"].sum().reset_index()
+# print(df_cat_app_install)
+
+# print(df_cat_app_install[df_cat_app_install["Category"]=="ART_AND_DESIGN"].sort_values(ascending=False,by="Installs")[ : 5]) # can be done same with all the others category
+
+
+apps = ['GAME','COMMUNICATION','PRODUCTIVITY','SOCIAL']
+sns.set_context("poster")
+sns.set_style("darkgrid")
+
+plt.figure(figsize=(10,30))
+
+for i,app in enumerate(apps):
+    df2 = df_cat_app_install[df_cat_app_install == app].sort_values(ascending=False,by="Installs")[:5]
+    df3 = df2.head(5)
+    plt.subplot(4,2,i+1)
+    sns.barplot(data=df3,x="Installs",y="App")
+    plt.xlabel('Installatioin in Millions')
+    plt.ylabel('')
+    plt.title(app,size = 20)
+
+plt.tight_layout()
+plt.subplots_adjust(hspace= .3)
+plt.show()
