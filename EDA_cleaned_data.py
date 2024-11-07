32ff03e086fa138976bbf93540318f7a16f5eba1
suddhu1 <pathaksudarshan27@gmail.com>
2024-11-08 01:03:40 +0545

    some knowledges

--- EDA_cleaned_data.py
+++ EDA_cleaned_data.py
@@ -1,21 +1,21 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-data = pd.read_csv("cleaned_googleplaystore_data.csv")
+# data = pd.read_csv("cleaned_googleplaystore_data.csv")
 # print(data )
 
-data_copy = data.copy()
-data_copy = data_copy.drop_duplicates(subset=['App'],keep="first")
-data_copy = data_copy.reset_index(drop=True)
+# data_copy = data.copy()
+# data_copy = data_copy.drop_duplicates(subset=['App'],keep="first")
+# data_copy = data_copy.reset_index(drop=True)
 
 # print(data_copy.info())
 # print(data_copy)
 
 # print(data_copy["Category"].unique())
-index_values = data_copy.index[data_copy['Category'] == '1.9'].tolist()
+# index_values = data_copy.index[data_copy['Category'] == '1.9'].tolist()
 # print(index_values)
-data_copy = data_copy.drop(data_copy.index[index_values])
-data_copy = data_copy.reset_index(drop=True)
+# data_copy = data_copy.drop(data_copy.index[index_values])
+# data_copy = data_copy.reset_index(drop=True)
 # print(data_copy.groupby(["Category"])["Installs"].sum().sort_values(ascending=False))  # to get the info about the category with the most instal
 
 
@@ -29,27 +29,27 @@ data_copy = data_copy.reset_index(drop=True)
 # plt.show()
 
 # top 5 app in the catagory with the most installs (multivariate analysis )
-df_cat_app_install  = data_copy.groupby(["Category","App"])["Installs"].sum().reset_index()
+# df_cat_app_install  = data_copy.groupby(["Category","App"])["Installs"].sum().reset_index()
 # print(df_cat_app_install)
 
 # print(df_cat_app_install[df_cat_app_install["Category"]=="ART_AND_DESIGN"].sort_values(ascending=False,by="Installs")[ : 5]) # can be done same with all the others category
 
 
-apps = ['GAME','COMMUNICATION','PRODUCTIVITY','SOCIAL']
-sns.set_context("poster")
-sns.set_style("darkgrid")
+# apps = ['GAME','COMMUNICATION','PRODUCTIVITY','SOCIAL']
+# sns.set_context("poster")
+# sns.set_style("darkgrid")
 
-plt.figure(figsize=(10,30))
+# plt.figure(figsize=(10,30))
 
-for i,app in enumerate(apps):
-    df2 = df_cat_app_install[df_cat_app_install == app].sort_values(ascending=False,by="Installs")[:5]
-    df3 = df2.head(5)
-    plt.subplot(4,2,i+1)
-    sns.barplot(data=df3,x="Installs",y="App")
-    plt.xlabel('Installatioin in Millions')
-    plt.ylabel('')
-    plt.title(app,size = 20)
+# for i,app in enumerate(apps):
+#     df2 = df_cat_app_install[df_cat_app_install == app].sort_values(ascending=False,by="Installs")[:5]
+#     df3 = df2.head(5)
+#     plt.subplot(4,2,i+1)
+#     sns.barplot(data=df3,x="Installs",y="App")
+#     plt.xlabel('Installatioin in Millions')
+#     plt.ylabel('')
+#     plt.title(app,size = 20)
 
-plt.tight_layout()
-plt.subplots_adjust(hspace= .3)
-plt.show()
+# plt.tight_layout()
+# plt.subplots_adjust(hspace= .3)
+# plt.show()
